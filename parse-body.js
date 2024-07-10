8bc443133a7d25e8aad7b4e844af4585daf0f6ce
Paul Wentzel <paul.wentzel@gmail.com>
2024-07-10 14:43:35 -0400

    practice complete

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,49 @@
 function firstStep(input) {
   // Your code here
+  return input.split('&');
 }
 
 function secondStep(input) {
   // Your code here
+  return input.map( e => e.split('='));
 }
 
 function thirdStep(input) {
   // Your code here
+  for (let i = 0; i < input.length; i++) {
+    for (j = 0; j < input[i].length; j++) {
+      if (input[i][j].includes('+')) {
+        input[i][j] = input[i][j].replace('+', ' ');
+      }
+    }
+  }
+  return input;
 }
 
 function fourthStep(input) {
   // Your code here
+  for (let i = 0; i < input.length; i++) {
+    for (j = 0; j < input[i].length; j++) {
+      input[i][j] = decodeURIComponent(input[i][j])
+    }
+  }
+  return input
 }
 
 function fifthStep(input) {
   // Your code here
+  const rv = {};
+  input.forEach(el => rv[el[0]] = el[1]);
+  return rv;
 }
 
 function parseBody(str) {
   // Your code here
+  let rv = firstStep(str);
+  rv = secondStep(rv);
+  rv = thirdStep(rv);
+  rv = fourthStep(rv);
+  return fifthStep(rv);
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -6,6 +6,29 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+const fs = require('fs');
+
+server = http.createServer((req, res) =>  {
+  console.log(req.method, req.url);
+
+  let reqBody = '';
+  req.on('data', (data) =>  {
+    reqBody += data;
+  });
+
+  req.on('end', () => {
+    if (reqBody) {
+      req.body = parseBody(reqBody);
+    }
+    sendFormPage(req, res);
+  })
+
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Successfully started the server on port', port));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
